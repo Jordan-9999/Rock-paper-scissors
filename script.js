5e5c1177c9938bdc1111f5e3c4b42e3d28b68b13
jordan <jsheeler90@gmail.com>
2022-07-06 21:58:52 -0400

    added function for round of game

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ return choices[Math.floor(Math.random()*choices.length)]
 
 let choices =["rock", "paper", "scissors"];
 console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection){
+switch(true){
+    case playerSelection == "rock" && computerSelection == "scissors":
+        result = "Rock beats scissors! You win!"
+        break;
+    case playerSelection == "rock" && computerSelection == "paper":
+        result = "Paper beats rock! You lose!"
+        break;
+    case playerSelection == "rock" && computerSelection == "rock":
+        result = "It's a draw! Try again!"
+        
+}
+
+
+
+return result
+}
+let result;
+let playerSelection = "rock";
+let computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
