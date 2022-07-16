3e86bc0b84802a8d76e9c82b8fbcf713fe8560ab
jordan <jsheeler90@gmail.com>
2022-07-16 07:49:46 -0400

    fixed the playRound(it was missing break in the switch statement) and added computerSelection to the loop

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-function computerPlay(){
+function getComputerChoice(){
 return choices[Math.floor(Math.random()*choices.length)]
 }
 
 let choices =["rock", "paper", "scissors"];
-console.log(computerPlay());
+//console.log(getComputerChoice());
+
+
+
 
 function playRound(playerSelection, computerSelection){
+
+   
+
 switch(true){
     case playerSelection == "rock" && computerSelection == "scissors":
         result = "Rock beats scissors! You win!"
@@ -15,15 +21,49 @@ switch(true){
         break;
     case playerSelection == "rock" && computerSelection == "rock":
         result = "It's a draw! Try again!"
-        
+        break;
+
+
+
+    case playerSelection == "paper" && computerSelection == "rock":
+        result = "Paper beats rock! You win!"
+        break;
+    case playerSelection == "paper" && computerSelection == "scissors":
+        result = "Scissors beats paper! You lose!"
+        break;
+    case playerSelection == "paper" && computerSelection == "paper":
+        result = "It's a draw! Try again!"
+        break;
+
+
+
+
+    case playerSelection == "scissors" && computerSelection == "paper":
+        result = "Scissors beats paper! You win!"
+        break;
+    case playerSelection == "scissors" && computerSelection == "rock":
+        result = "Rock beats scissors! You lose!"
+        break;
+    case playerSelection == "scissors" && computerSelection == "scissors":
+        result = "It's a draw! Try again!"
 }
 
 
 
 return result
-}
-let result;
-let playerSelection = "rock";
-let computerSelection = computerPlay();
 
-console.log(playRound(playerSelection, computerSelection));
+}
+
+    let result = "";
+    const computerSelection = getComputerChoice();
+
+//ok this seems to work! ill will do more work on it later...
+//console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+
+for(let i = 0; i < 5; i++){
+    let playerSelection = prompt();
+    const computerSelection = getComputerChoice();
+    
+    alert(result);
+    console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+}
