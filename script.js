5ee7ef3b44e00266b02e54043254a03385a24835
jordan <jsheeler90@gmail.com>
2024-01-03 21:14:50 -0500

    add script.js

--- script.js
+++ script.js
@@ -11,76 +11,72 @@ return choices[Math.floor(Math.random()*choices.length)]
 
 
 
-function playRound(playerSelection, computerSelection){
-switch(true){
-    case playerSelection == "rock" && computerSelection == "scissors":
-        playRoundResult = "Rock beats scissors! You win!",
-        playerScore++;
-        break;
-    case playerSelection == "rock" && computerSelection == "paper":
-        playRoundResult = "Paper beats rock! You lose!",
-        computerScore++;
-        break;
-    case playerSelection == "rock" && computerSelection == "rock":
-        playRoundResult = "It's a draw!"
-        break;
-
-
-
-    case playerSelection == "paper" && computerSelection == "rock":
-        playRoundResult = "Paper beats rock! You win!",
-        playerScore++;
-        break;
-    case playerSelection == "paper" && computerSelection == "scissors":
-        playRoundResult = "Scissors beats paper! You lose!",
-        computerScore++;
-        break;
-    case playerSelection == "paper" && computerSelection == "paper":
-        playRoundResult = "It's a draw!"
-        break;
-
-
-
-
-    case playerSelection == "scissors" && computerSelection == "paper":
-        playRoundResult = "Scissors beats paper! You win!"
-        playerScore++;
-        break;
-    case playerSelection == "scissors" && computerSelection == "rock":
-        playRoundResult = "Rock beats scissors! You lose!",
-        computerScore++;
-        break;
-    case playerSelection == "scissors" && computerSelection == "scissors":
-        playRoundResult = "It's a draw!"
-}
-
-return playRoundResult
-}
-/*In the playRound function we have switch statement. Depending on the outcome of a round, there is different message displayed and the score of
-either the player or computer will go up by one point (playerScore++) unless it's a tie. */ 
-
-    let choices =["rock", "paper", "scissors"];
-
-    let playerScore = 0;
-    let computerScore = 0;
+  
+let choices =["rock", "paper", "scissors"];
     
-    let playRoundResult = "";
-    let endOfGameMessage = "";
-   
-    
+let playerScore = 0;
+let computerScore = 0;
+
+let playRoundResult = "";
+let endOfGameMessage = "";
 
 
 
 function game(){
 
+        function playRound(playerSelection, computerSelection){
+         switch(true){
+              case playerSelection == "rock" && computerSelection == "scissors":
+                   playRoundResult = "Rock beats scissors! You win!",
+                   playerScore++;
+                   break;
+              case playerSelection == "rock" && computerSelection == "paper":
+                   playRoundResult = "Paper beats rock! You lose!",
+                   computerScore++;
+                    break;
+              case playerSelection == "rock" && computerSelection == "rock":
+                   playRoundResult = "It's a draw!"
+                   break;
+        
 
-  for(let i = 0; i < 5; i++){
+              case playerSelection == "paper" && computerSelection == "rock":
+                   playRoundResult = "Paper beats rock! You win!",
+                   playerScore++;
+                   break;
+              case playerSelection == "paper" && computerSelection == "scissors":
+                   playRoundResult = "Scissors beats paper! You lose!",
+                   computerScore++;
+                   break;
+              case playerSelection == "paper" && computerSelection == "paper":
+                   playRoundResult = "It's a draw!"
+                   break;
+        
+        
+              case playerSelection == "scissors" && computerSelection == "paper":
+                   playRoundResult = "Scissors beats paper! You win!"
+                   playerScore++;
+                   break;
+              case playerSelection == "scissors" && computerSelection == "rock":
+                   playRoundResult = "Rock beats scissors! You lose!",
+                   computerScore++;
+                   break;
+              case playerSelection == "scissors" && computerSelection == "scissors":
+                   playRoundResult = "It's a draw!"
+                   }
+        
+        return playRoundResult
+        }
+
+        /*In the playRound function we have switch statement. Depending on the outcome of a round, there is different message displayed and the score of
+        either the player or computer will go up by one point (playerScore++) unless it's a tie. */
+
+
+
+
+        for(let i = 0; i < 5; i++){
     const computerSelection = getComputerChoice();
     let playerSelection = prompt();
     alert(playRound(playerSelection.toLowerCase(), computerSelection)); 
-   // if (!playerScore++ && !computerScore++)  {
-   //     i--
- //}
 }
 
 
