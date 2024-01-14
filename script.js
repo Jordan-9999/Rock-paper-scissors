daab5d556cfc8abab3f6f28ef3b528ba74e1da4b
jordan <jsheeler90@gmail.com>
2024-01-13 20:16:56 -0500

    Add script.js

--- script.js
+++ script.js
@@ -77,6 +77,9 @@ function game(){
     const computerSelection = getComputerChoice();
     let playerSelection = prompt();
     alert(playRound(playerSelection.toLowerCase(), computerSelection)); 
+    if(playRoundResult == "It's a draw!"){
+        i--
+    }
 }
 
 
